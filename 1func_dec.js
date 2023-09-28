a1364184b3685b0fbccc9171f51a9dae8755d5ac
Hafiz Manfaz Ali <manfazali4@gmail.com>
2023-09-28 22:55:58 +0500

    first commit

---
+++ 1func_dec.js
@@ -0,0 +1,8 @@
+// 1- function declaration d 
+// this type of function known as function declaration in technical term
+function function_name()
+{
+    return 2;
+}
+
+console.log(function_name());
